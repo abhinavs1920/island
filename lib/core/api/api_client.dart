e68c997de95c1f1c42eb28311dbde827d7014e39
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2026-08-28 01:40:56 +0530

    chore: set default API base URL to real server IP

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -15,7 +15,7 @@ class ApiClient {
   late Dio _dio;
 
   ApiClient(this._storage, this._router) {
-    const baseUrl = String.fromEnvironment('BASE_URL', defaultValue: 'http://10.0.2.2:8000');
+    const baseUrl = String.fromEnvironment('BASE_URL', defaultValue: 'http://20.119.84.223');
     _dio = Dio(BaseOptions(
       baseUrl: baseUrl,
       connectTimeout: const Duration(seconds: 10),
